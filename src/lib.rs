778acf8dd519f48b441716bc6e885126c8f3b483
LexiBigCheese <ajclarke903@gmail.com>
2025-06-16 17:17:40 +0100

    Maybe this will work bettter

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,10 @@ pub struct JigglePropagation<Loc, Dir> {
     pub came_from: Dir,
 }
 
+pub trait MutCol<Loc> {
+    fn y_slime(&mut self, f: impl FnMut(usize, SlimePropsIn<Loc>) -> SlimePropsOut);
+}
+
 pub trait JigglyBoard {
     type Dir: Direction + Copy + Clone;
     type Loc: Copy + Clone;
@@ -72,9 +76,8 @@ pub trait JigglyBoard {
         loc: Self::Loc,
         impulse: f32,
     ) -> Option<(Self::Loc, f32)>;
-    fn cols_mut(
-        &mut self,
-    ) -> impl IntoIterator<Item = impl IntoIterator<Item = impl MutSlime<Self::Loc>>>;
+    type Col: MutCol<Self::Loc>;
+    fn cols_mut(&mut self, f: impl FnMut(Self::Col));
     fn impulse_jiggle_at(&mut self, loc: Self::Loc) -> Option<impl JiggleImpulsable>;
 
     /// If this returns true, the board is settled
@@ -82,109 +85,107 @@ pub trait JigglyBoard {
         let mut jiggle_propagations: alloc::vec::Vec<JigglePropagation<Self::Loc, Self::Dir>> =
             alloc::vec![];
         let mut settled = true;
-        for col in self.cols_mut().into_iter() {
+        self.cols_mut(|mut col| {
             let mut jiggle_offset = 0.0;
-            for (y, mut slime) in col.into_iter().enumerate() {
-                slime.modify_slime(
-                    |SlimePropsIn {
-                         state,
-                         y_bottom,
-                         location,
-                     }| {
-                        use SlimeState::*;
-                        match state {
-                            Settled => {
-                                let out = SlimePropsOut {
-                                    state,
-                                    y_bottom: jiggle_offset,
-                                    y_scale: 1.0,
-                                    x_scale: 1.0,
-                                };
-                                jiggle_offset += 1.0;
-                                out
-                            }
-                            Falling { velocity } => {
-                                settled = false;
-                                let velocity = velocity + dt * physprop.gravity;
-                                let y_bottom = y_bottom - velocity * dt;
-                                if y_bottom <= y as f32 {
-                                    jiggle_propagations.push(JigglePropagation {
-                                        at: location,
-                                        impulse: physprop.velocity_to_impact * velocity,
-                                        came_from: Self::Dir::UP,
-                                    });
-                                    let y_bottom = jiggle_offset;
-                                    jiggle_offset += 1.0;
-                                    SlimePropsOut {
-                                        state: Jiggling {
-                                            momentum: 0.0,
-                                            offset: 0.0,
-                                            life: 1.0,
-                                        },
-                                        x_scale: 1.0,
-                                        y_scale: 1.0,
-                                        y_bottom,
-                                    }
-                                } else {
-                                    let clamped_vel =
-                                        velocity.mul(1.0 / 9.0).add(1.0).clamp(1.0, 2.0);
-                                    let x_scale = 1.0 / clamped_vel;
-                                    let y_scale = 1.0 * clamped_vel;
-                                    SlimePropsOut {
-                                        state: Falling { velocity },
-                                        y_bottom,
-                                        y_scale,
-                                        x_scale,
-                                    }
-                                }
-                            }
-                            Jiggling {
-                                momentum,
-                                offset,
-                                life,
-                            } => {
-                                settled = false;
+            col.y_slime(
+                |y,
+                 SlimePropsIn {
+                     state,
+                     y_bottom,
+                     location,
+                 }| {
+                    use SlimeState::*;
+                    match state {
+                        Settled => {
+                            let out = SlimePropsOut {
+                                state,
+                                y_bottom: jiggle_offset,
+                                y_scale: 1.0,
+                                x_scale: 1.0,
+                            };
+                            jiggle_offset += 1.0;
+                            out
+                        }
+                        Falling { velocity } => {
+                            settled = false;
+                            let velocity = velocity + dt * physprop.gravity;
+                            let y_bottom = y_bottom - velocity * dt;
+                            if y_bottom <= y as f32 {
+                                jiggle_propagations.push(JigglePropagation {
+                                    at: location,
+                                    impulse: physprop.velocity_to_impact * velocity,
+                                    came_from: Self::Dir::UP,
+                                });
                                 let y_bottom = jiggle_offset;
-                                let accdt = physprop.jiggle_stiff * -offset * dt;
-                                let mut momentum = (momentum + accdt) * physprop.jiggle_damp;
-                                let mut offset = offset + momentum * dt;
-                                if life < physprop.jiggle_life_threshold {
-                                    offset *= life * physprop.jiggle_life_threshold_inverse;
-                                    momentum *= life * physprop.jiggle_life_threshold_inverse;
+                                jiggle_offset += 1.0;
+                                SlimePropsOut {
+                                    state: Jiggling {
+                                        momentum: 0.0,
+                                        offset: 0.0,
+                                        life: 1.0,
+                                    },
+                                    x_scale: 1.0,
+                                    y_scale: 1.0,
+                                    y_bottom,
                                 }
-                                if (life <= 0.0)
-                                    || (offset.abs() < physprop.jiggle_offset_epsilon
-                                        && momentum.abs() < physprop.jiggle_momentum_epsilon)
-                                {
-                                    jiggle_offset += 1.0;
-                                    SlimePropsOut {
-                                        state: Settled,
-                                        y_bottom,
-                                        y_scale: 1.0,
-                                        x_scale: 1.0,
-                                    }
-                                } else {
-                                    let life = life - physprop.jiggle_life_decrease_rate * dt;
-                                    let y_scale = (-offset).max(-1.0);
-                                    let x_scale = y_scale.max(0.5).recip();
-                                    jiggle_offset += y_scale;
-                                    SlimePropsOut {
-                                        state: Jiggling {
-                                            momentum,
-                                            offset,
-                                            life,
-                                        },
-                                        x_scale,
-                                        y_scale,
-                                        y_bottom,
-                                    }
+                            } else {
+                                let clamped_vel = velocity.mul(1.0 / 9.0).add(1.0).clamp(1.0, 2.0);
+                                let x_scale = 1.0 / clamped_vel;
+                                let y_scale = 1.0 * clamped_vel;
+                                SlimePropsOut {
+                                    state: Falling { velocity },
+                                    y_bottom,
+                                    y_scale,
+                                    x_scale,
                                 }
                             }
                         }
-                    },
-                );
-            }
-        }
+                        Jiggling {
+                            momentum,
+                            offset,
+                            life,
+                        } => {
+                            settled = false;
+                            let y_bottom = jiggle_offset;
+                            let accdt = physprop.jiggle_stiff * -offset * dt;
+                            let mut momentum = (momentum + accdt) * physprop.jiggle_damp;
+                            let mut offset = offset + momentum * dt;
+                            if life < physprop.jiggle_life_threshold {
+                                offset *= life * physprop.jiggle_life_threshold_inverse;
+                                momentum *= life * physprop.jiggle_life_threshold_inverse;
+                            }
+                            if (life <= 0.0)
+                                || (offset.abs() < physprop.jiggle_offset_epsilon
+                                    && momentum.abs() < physprop.jiggle_momentum_epsilon)
+                            {
+                                jiggle_offset += 1.0;
+                                SlimePropsOut {
+                                    state: Settled,
+                                    y_bottom,
+                                    y_scale: 1.0,
+                                    x_scale: 1.0,
+                                }
+                            } else {
+                                let life = life - physprop.jiggle_life_decrease_rate * dt;
+                                let y_scale = (-offset).max(-1.0);
+                                let x_scale = y_scale.max(0.5).recip();
+                                jiggle_offset += y_scale;
+                                SlimePropsOut {
+                                    state: Jiggling {
+                                        momentum,
+                                        offset,
+                                        life,
+                                    },
+                                    x_scale,
+                                    y_scale,
+                                    y_bottom,
+                                }
+                            }
+                        }
+                    }
+                },
+            );
+        });
         //Now run through jiggle propagations
         for propagation in jiggle_propagations {
             self.propagate_jiggle(propagation, physprop);
